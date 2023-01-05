d8f9c0d12ad313ce5746e1e7ceccde40fd149bb0
saeed.alim_03 <93273931+Alim003@users.noreply.github.com>
2023-01-05 13:21:07 +0530

    Create Ticket collector .py

---
+++ Ticket collector .py
@@ -0,0 +1,15 @@
+print("Welcome to the rollercoaster!")
+height = int(input("what is your height in cm?"))
+
+if height >= 120:
+print("You can ride the rollercoaster!")
+age = is your age?
+if
+age < 12:
+print("Please pay $5." )
+elif age 181'
+print("Please pay $7." )
+else:
+print ("Please pay $12.
+else:
+print("Sorry, you have to grow taller before you can ride. ")
