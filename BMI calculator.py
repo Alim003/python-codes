60c3cd8edc4cf819ed995c099ca95564c044408b
saeed.alim_03 <93273931+Alim003@users.noreply.github.com>
2023-01-05 19:28:19 +0530

    Update BMI calculator.py

--- BMI calculator.py
+++ BMI calculator.py
@@ -4,10 +4,12 @@ weight = float(input("enter your weight in kg: "))
 bmi = round(weight / height **2)
 
 if bmi < 18.5:
-    print(f"your bmi is {bmi} , you are underweight.")
+    print(f"your BMI is {bmi}, you are underweight.")
 elif bmi < 25:
-    print(f"your bmi is {bmi} , you have a normal weight.")
+    print(f"your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+    print(f"your BMI is {bmi}, you are slightly overweight.")    
 elif bmi < 35:
-    print(f"your bmi is {bmi} , you are obese.")
+    print(f"your BMI is {bmi}, you are obese.")
 else:
-    print(f"your bmi is {bmi} , you are clinically obese.")    
+    print(f"your BMI is {bmi}, you are clinically obese.")     
